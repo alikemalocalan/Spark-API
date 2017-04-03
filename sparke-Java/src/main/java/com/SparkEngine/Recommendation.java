790a0d9a8f3c38419c3a6bb7fea452206a468639
alikemal <alikemalocalan@gmail.com>
2017-04-04 02:04:02 +0300

    added java logging to mongo

--- sparke-Java/src/main/java/com/SparkEngine/Recommendation.java
+++ sparke-Java/src/main/java/com/SparkEngine/Recommendation.java
@@ -4,9 +4,13 @@ package com.SparkEngine;
  * Created by alikemal on 19.03.2017.
  */
 
-import com.model.Line;
+import com.model.Song;
+import com.model.Tag;
+import com.model.UserSales;
+import com.model.UserStat;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -17,7 +21,12 @@ import java.util.List;
 
 public class Recommendation {
     private static final Logger LOGGER = LoggerFactory.getLogger(Recommendation.class);
-    private static final String tracks_txt = "/home/alikemal/IdeaProjects/Spark-API/dataset/unique_tracks.txt";
+    private static final String base_txt = "/home/alikemal/IdeaProjects/Spark-API/dataset/";
+
+    private static final String tag_txt = base_txt + "msd-MAGD-genreAssignment-new.cls";
+    private static final String price_txt = base_txt + "train_triplets.txtaa";
+    private static final String userStat_txt = base_txt + "jam_to_msd-new.tsv";
+    private static final String tracks_txt = base_txt + "unique_tracks.txt";
     private static final String master = "local[*]";
     private SparkSession spark = null;
 
@@ -32,23 +41,98 @@ public class Recommendation {
                 .config(conf)
                 .getOrCreate();
 
-        JavaRDD<Line> lineRDD = spark.read()
+        //******************************************
+
+
+        JavaRDD<Song> songRDD = spark.read()
                 .textFile(tracks_txt)
                 .javaRDD()
                 .map(lineRAW -> {
                     String[] parts = lineRAW.split(",");
-                    Line line = new Line();
-                    line.setArtistTitle(parts[2]);
-                    line.setSongTitle(parts[3]);
+                    Song line = new Song();
+                    line.setTrackID(parts[0]);
+                    line.setSongID(parts[1]);
+                    line.setSongTitle(parts[2]);
+                    line.setArtistTitle(parts[3]);
                     return line;
                 });
 
-        Dataset<Row> lineDF = spark.createDataFrame(lineRDD, Line.class);
-        lineDF.createOrReplaceTempView("song");
+        Dataset<Row> songDF = spark.createDataFrame(songRDD, Song.class);
+        songDF.createOrReplaceTempView("song");
+
+        //******************************************
+
+
+        JavaRDD<UserSales> songPriceRDD = (JavaRDD<UserSales>) spark.read()
+                .textFile(price_txt)
+                .javaRDD()
+                .map(lineRAW -> {
+                    String[] parts = lineRAW.split(",");
+                    UserSales price = new UserSales();
+                    price.setUserID(parts[0]);
+                    price.setTrackID(parts[1]);
+                    price.setPrice(parts[2]);
+                    return price;
+                });
+
+        Dataset<Row> songPriceDF = spark.createDataFrame(songPriceRDD, UserSales.class);
+        songPriceDF.createOrReplaceTempView("usersongPrice");
+
+
+        //******************************************
+
+
+        JavaRDD<Tag> tagRDD = spark.read()
+                .textFile(tag_txt)
+                .javaRDD()
+                .map(lineRAW -> {
+                    String[] parts = lineRAW.split(",");
+                    Tag tag = new Tag();
+                    tag.setTrackID(parts[0]);
+                    tag.setTagID(parts[1]);
+                    return tag;
+                });
+
+        Dataset<Row> tagDF = spark.createDataFrame(tagRDD, Tag.class);
+        tagDF.createOrReplaceTempView("tag");
+
+
+        //******************************************
+
+
+        JavaRDD<UserStat> userRDD = spark.read()
+                .textFile(userStat_txt)
+                .javaRDD()
+                .map(lineRAW -> {
+                    String[] parts = lineRAW.split(",");
+                    UserStat stat = new UserStat();
+                    stat.setUserID(parts[0]);
+                    stat.setTrackID(parts[1]);
+                    return stat;
+                });
+
+        Dataset<Row> userDf = spark.createDataFrame(userRDD, UserStat.class);
+        userDf.createOrReplaceTempView("userStat");
     }
 
     public List<String> getSongs() {
         Dataset<Row> result = spark.sql("SELECT * FROM song limit 1");
+        return result.toJSON().collectAsList();
+    }
+
+    public String getSongbyTrackID(String trackID) {
+        Dataset<Row> result = spark.sql("SELECT * FROM song limit 1").filter(new Column("trackID").equalTo(trackID));
+        return result.toJSON().collectAsList().get(0);
+    }
+
+    public List<String> getStats() {
+        Dataset<Row> result = spark.sql("SELECT * " +
+                "FROM userStat st" +
+                " INNER JOIN tag t ON" +
+                " st.trackID= t.trackID " +
+                " INNER JOIN song p ON" +
+                " t.trackID= p.trackID " +
+                "limit 10");
         return result.toJSON().collectAsList();
     }
 }
--- sparke-Java/src/main/java/com/controller/SongController.java
+++ sparke-Java/src/main/java/com/controller/SongController.java
@@ -1,21 +1,34 @@
 package com.controller;
 
 import com.SparkEngine.Recommendation;
+import com.logging.LogMong;
 import spark.Request;
 import spark.Response;
-import spark.Route;
 
+import static spark.Spark.before;
 import static spark.Spark.get;
 
 /**
  * Created by alikemal on 19.03.2017.
  */
 public class SongController {
+    private final LogMong loging= new LogMong();
 
     public SongController(final Recommendation sparkService) {
-        get("/songs", (request, response) -> {
+        before("/*", (Request request, Response response) -> {
             response.type("application/json");
-            return new Recommendation().getSongs();
+
+        });
+        get("/songs", (request, response) -> {
+                        return sparkService.getSongs();
+        });
+
+        get("/userstat", (request, response) -> {
+            return sparkService.getStats();
+        });
+        get("/getsong", (request, response) -> {
+            loging.insertUserStat( request.queryMap().get("userID").value(), request.queryMap().get("trackID").value());
+            return sparkService.getSongbyTrackID("TRMMMYQ128F932D901");
         });
     }
 }
